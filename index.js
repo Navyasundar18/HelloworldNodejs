346480c9ac299a9bdc71d1ba11cd8ce3e9cbb314
Ankayarkanni Subbannan <ankayarkanni_subbannan@thbs.india.com>
2019-09-25 12:26:09 +0530

    updates hello world index js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ var server = http.createServer(function(request, response) {
 
 var port = 80;
 server.listen(port);
-
+console.log("etsting auto build by jenknins");
 console.log("Server running at http://localhost:%d", port);
