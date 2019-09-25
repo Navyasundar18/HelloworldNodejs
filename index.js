31b308dabf00deacf6b825c2a6c3f69fe4c1c60b
devopss2019 <53856355+devopss2019@users.noreply.github.com>
2019-09-25 12:21:12 +0530

    Update index.js
    simple commit

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ var server = http.createServer(function(request, response) {
 
 var port = 80;
 server.listen(port);
-
+console.log("testing auto build........");
 console.log("Server running at http://localhost:%d", port);
