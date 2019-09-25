0cb27a08b29811c28781a11610c01ff477744d5e
Ankayarkanni Subbannan <ankayarkanni_subbannan@thbs.india.com>
2019-09-25 16:05:17 +0530

    update 5

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ var server = http.createServer(function(request, response) {
 var port = 80;
 server.listen(port);
 console.log("=============================");
-console.log("new changes");
-console.log("------latest changes");
-console.log("----------------------------******************");
-console.log("i---------------------testing auto build........");
 console.log("*******************");
 console.log("Server running at http://localhost:%d", port);
-
+console.log("---------------------------");
